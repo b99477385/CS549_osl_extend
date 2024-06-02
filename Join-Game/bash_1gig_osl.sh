13a125e519ed3ca2c9967f9b33c47744e004e3c2
YuYang Lin <linyuya@oregonstate.edu>
2024-06-02 15:33:01 -0700

    test

---
+++ Join-Game/bash_1gig_osl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# python new_script_1gig_osl_q10.py full_1gig_osl_q10 sum_1gig_osl_q10
+# python new_script_1gig_osl_q12.py full_1gig_osl_q12 sum_1gig_osl_q12
+# python new_script_1gig_osl_q2.py full_1gig_osl_q2 sum_1gig_osl_q2
+python 1gig_osl_q10nolimit.py full_1gig_osl_q10 sum_1gig_osl_q10
